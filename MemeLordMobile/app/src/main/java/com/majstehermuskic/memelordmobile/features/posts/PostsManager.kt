b50fc75e00fd9508a797a23adf596497a63a362f
Bartosz Majewski <bartmaj@windowslive.com>
2018-03-20 20:45:34 +0100

    MB - infinite scroll - kotlin -> infinite scroll ukończony.

--- MemeLordMobile/app/src/main/java/com/majstehermuskic/memelordmobile/features/posts/PostsManager.kt
+++ MemeLordMobile/app/src/main/java/com/majstehermuskic/memelordmobile/features/posts/PostsManager.kt
@@ -2,21 +2,27 @@ package com.majstehermuskic.memelordmobile.features.posts
 
 import com.majstehermuskic.memelordmobile.api.RestAPI
 import com.majstehermuskic.memelordmobile.commons.MemePostItem
+import com.majstehermuskic.memelordmobile.commons.MemePosts
 import io.reactivex.Observable
 
 class PostsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getPosts(last: Int = 0, count: Int = 10): Observable<List<MemePostItem>> {
+    fun getPosts(last: Int = 0, count: Int = 10): Observable<MemePosts> {
         return Observable.create {
             subscriber ->
             val callResponse = api.getPosts(last, count)
             val response = callResponse.execute()
 
             if(response.isSuccessful) {
-                val posts = response.body()!!.posts.map {
+                val postsResponse = response.body()
+                val posts = postsResponse!!.posts.map {
                     MemePostItem(it.title, it.image)
                 }
-                subscriber.onNext(posts)
+                val memePosts = MemePosts(
+                        postsResponse.last,
+                        posts
+                )
+                subscriber.onNext(memePosts)
                 subscriber.onComplete()
             } else {
                 subscriber.onError(Throwable(response.message()))
