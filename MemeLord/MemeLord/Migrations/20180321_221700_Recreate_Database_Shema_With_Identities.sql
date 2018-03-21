4481feb52d5e328c46bfd021303014ada08736bb
Mateusz Herjan <mateusz624@student.polsl.pl>
2018-03-21 22:22:13 +0100

    GENERAL, BE - wystawienie akcji api do logowania i tworzenia nowego użytkownika -> IMPORTANT Added migration with recreation of database due to lacking identities on nearly every database entity. Also updated with some unique fields. Salt -> Hash renamed misnamed column for Users.

---
+++ MemeLord/MemeLord/Migrations/20180321_221700_Recreate_Database_Shema_With_Identities.sql
@@ -0,0 +1,272 @@
+﻿DROP DATABASE MemeLordDb
+GO
+CREATE DATABASE MemeLordDb
+GO
+USE MemeLordDb
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Comments](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PostId] [int] NOT NULL,
+	[MasterCommentId] [int] NULL,
+	[UserId] [int] NOT NULL,
+	[Rating] [int] NOT NULL CONSTRAINT DF_Comments_Rating DEFAULT 0,
+	[DeletionDate] [datetime] NULL,
+	[CreationDate] [datetime] NOT NULL,
+	[Text] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Following]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Followings](
+	[FollowerId] [int] NOT NULL,
+	[FollowedId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Likes]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Likes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[CreationDate] [datetime] NOT NULL,
+	[PostId] [int] NULL,
+	[CommentId] [int] NULL,
+	[Value] [int] NOT NULL CONSTRAINT DF_Likes_Value DEFAULT 0,
+ CONSTRAINT [PK_Likes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[NotificationDisplayed]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[NotificationTypes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_NotificationDisplayed] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Notifications]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Notifications](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[NotificationTypeId] [int] NOT NULL,
+	[UserId] [int] NULL,
+	[PostId] [int] NULL,
+	[WasDisplayed] [bit] NOT NULL,
+ CONSTRAINT [PK_Notifications] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Posts]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Posts](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[OpId] [int] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[Image] [nvarchar](max) NOT NULL,
+	[CreationDate] [datetime] NOT NULL,
+	[DeletionDate] [datetime] NULL,
+	[Rating] [int] NOT NULL CONSTRAINT DF_Posts_Rating DEFAULT 0,
+ CONSTRAINT [PK_Posts] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ReportDescription]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[ReportTypes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_ReportDescription] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Reports]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Reports](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PostId] [int] NULL,
+	[CommentId] [int] NULL,
+	[ReporterId] [int] NOT NULL,
+	[ReportDate] [datetime] NOT NULL,
+	[ReportTypeId] [int] NOT NULL,
+ CONSTRAINT [PK_Reports] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[roles]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [AK_Roles_Name] UNIQUE([Name]),
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserRoles]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRoles](
+	[UserId] [int] NOT NULL,
+	[RoleId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2018-03-14 21:12:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IdENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](30) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[Hash] [nvarchar](max) NOT NULL,
+	[Avatar] [nvarchar](max) NULL,
+	[Sex] [nvarchar](15) NULL,
+	[DateOfBirth] [datetime] NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [AK_Users_Username] UNIQUE([Username]), 
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Comments] FOREIGN KEY([MasterCommentId])
+REFERENCES [dbo].[Comments] ([Id])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Comments]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Posts] FOREIGN KEY([PostId])
+REFERENCES [dbo].[Posts] ([Id])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Posts]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Users]
+GO
+ALTER TABLE [dbo].[Followings]  WITH CHECK ADD  CONSTRAINT [FK_Following_Users] FOREIGN KEY([FollowerId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Followings] CHECK CONSTRAINT [FK_Following_Users]
+GO
+ALTER TABLE [dbo].[Followings]  WITH CHECK ADD  CONSTRAINT [FK_Following_Users1] FOREIGN KEY([FollowedId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Followings] CHECK CONSTRAINT [FK_Following_Users1]
+GO
+ALTER TABLE [dbo].[Likes]  WITH CHECK ADD  CONSTRAINT [FK_Likes_Comments] FOREIGN KEY([CommentId])
+REFERENCES [dbo].[Comments] ([Id])
+GO
+ALTER TABLE [dbo].[Likes] CHECK CONSTRAINT [FK_Likes_Comments]
+GO
+ALTER TABLE [dbo].[Likes]  WITH CHECK ADD  CONSTRAINT [FK_Likes_Posts] FOREIGN KEY([PostId])
+REFERENCES [dbo].[Posts] ([Id])
+GO
+ALTER TABLE [dbo].[Likes] CHECK CONSTRAINT [FK_Likes_Posts]
+GO
+ALTER TABLE [dbo].[Likes]  WITH CHECK ADD  CONSTRAINT [FK_Likes_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Likes] CHECK CONSTRAINT [FK_Likes_Users]
+GO
+ALTER TABLE [dbo].[Notifications]  WITH CHECK ADD  CONSTRAINT [FK_Notifications_NotificationTypes] FOREIGN KEY([NotificationTypeId])
+REFERENCES [dbo].[NotificationTypes] ([Id])
+GO
+ALTER TABLE [dbo].[Notifications] CHECK CONSTRAINT [FK_Notifications_NotificationTypes]
+GO
+ALTER TABLE [dbo].[Notifications]  WITH CHECK ADD  CONSTRAINT [FK_Notifications_Posts] FOREIGN KEY([PostId])
+REFERENCES [dbo].[Posts] ([Id])
+GO
+ALTER TABLE [dbo].[Notifications] CHECK CONSTRAINT [FK_Notifications_Posts]
+GO
+ALTER TABLE [dbo].[Notifications]  WITH CHECK ADD  CONSTRAINT [FK_Notifications_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Notifications] CHECK CONSTRAINT [FK_Notifications_Users]
+GO
+ALTER TABLE [dbo].[Posts]  WITH CHECK ADD  CONSTRAINT [FK_Posts_Users] FOREIGN KEY([OpId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Posts] CHECK CONSTRAINT [FK_Posts_Users]
+GO
+ALTER TABLE [dbo].[Reports]  WITH CHECK ADD  CONSTRAINT [FK_Reports_Comments] FOREIGN KEY([CommentId])
+REFERENCES [dbo].[Comments] ([Id])
+GO
+ALTER TABLE [dbo].[Reports] CHECK CONSTRAINT [FK_Reports_Comments]
+GO
+ALTER TABLE [dbo].[Reports]  WITH CHECK ADD  CONSTRAINT [FK_Reports_Posts] FOREIGN KEY([PostId])
+REFERENCES [dbo].[Posts] ([Id])
+GO
+ALTER TABLE [dbo].[Reports] CHECK CONSTRAINT [FK_Reports_Posts]
+GO
+ALTER TABLE [dbo].[Reports]  WITH CHECK ADD  CONSTRAINT [FK_Reports_ReportTypes] FOREIGN KEY([ReportTypeId])
+REFERENCES [dbo].[ReportTypes] ([Id])
+GO
+ALTER TABLE [dbo].[Reports] CHECK CONSTRAINT [FK_Reports_ReportTypes]
+GO
+ALTER TABLE [dbo].[Reports]  WITH CHECK ADD  CONSTRAINT [FK_Reports_Users] FOREIGN KEY([ReporterId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Reports] CHECK CONSTRAINT [FK_Reports_Users]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_roles] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[roles] ([Id])
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_roles]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_Users]
+GO
