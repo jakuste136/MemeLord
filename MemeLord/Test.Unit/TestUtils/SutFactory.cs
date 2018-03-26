630b7bb468190eec01becdcb6d5fdd5f29415c9b
Kuba Stepaniak <jakuste136@student.polsl.pl>
2018-03-26 21:16:09 +0200

    BE -> dodano autofixture i moq.

---
+++ MemeLord/Test.Unit/TestUtils/SutFactory.cs
@@ -0,0 +1,12 @@
+﻿using AutoFixture;
+
+namespace Test.Unit.TestUtils
+{
+    public static class SutFactory
+    {
+        public static T CreateSut<T>() where T : class
+        {
+            return new Fixture().Create<T>();
+        }
+    }
+}
