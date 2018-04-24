4b102af415b20205b87658c542d43ce3ef7f2288
Mateusz Herjan <mateusz624@student.polsl.pl>
2018-04-25 00:26:06 +0200

    BE -> Claims Wrapper added, updated usage. Update user mapping altered. User controller cleanup.

---
+++ MemeLord/MemeLord/Logic/Extensions/ClaimsPrincipalWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace MemeLord.Logic.Extensions
+{
+    public static class ClaimsPrincipalWrapper
+    {
+        public static string GetFromClaim(string types)
+        {
+            return ClaimsPrincipal.Current.Claims.FirstOrDefault(x => x.Type == types)?.Value ?? "";
+        }
+    }
+}
--- MemeLord/MemeLord/Logic/Modules/Users/UserGetModule.cs
+++ MemeLord/MemeLord/Logic/Modules/Users/UserGetModule.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Security.Claims;
 using MemeLord.DataObjects.Response;
+using MemeLord.Logic.Extensions;
 using MemeLord.Logic.Mapping.Users;
 using MemeLord.Logic.Repository;
 
@@ -8,6 +10,7 @@ namespace MemeLord.Logic.Modules.Users
     public interface IUserGetModule
     {
         GetUserResponse GetUserById(int id);
+        GetUserResponse GetSelf();
         GetUserResponse GetUserByName(string name);
         IList<GetUserResponse> GetAllUsers();
     }
@@ -26,6 +29,15 @@ namespace MemeLord.Logic.Modules.Users
         public GetUserResponse GetUserById(int id)
         {
             var user = _userRepository.GetUserById(id);
+            return _responseMapper.Map(user);
+        }
+
+        public GetUserResponse GetSelf()
+        {
+            var username = ClaimsPrincipalWrapper.GetFromClaim(ClaimTypes.Name);
+            if (string.IsNullOrEmpty(username))
+                return null;
+            var user = _userRepository.GetUserByCredentials(username);
             return _responseMapper.Map(user);
         }
 
