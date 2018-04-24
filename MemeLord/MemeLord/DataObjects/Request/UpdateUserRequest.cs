d24f74d1b9486782fdcb38d8834ff217fb3343f4
Mateusz Herjan <mateusz624@student.polsl.pl>
2018-04-24 20:23:57 +0200

    BE -> Updated user controller to support getting users by username, self-get. Update user by UpdateUserRequest instead of json patch.

---
+++ MemeLord/MemeLord/DataObjects/Request/UpdateUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MemeLord.Models;
+
+namespace MemeLord.DataObjects.Request
+{
+    public class UpdateUserRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public Sex Sex { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Description { get; set; }
+    }
+}
--- MemeLord/MemeLord/Logic/Modules/Users/UserUpdateModule.cs
+++ MemeLord/MemeLord/Logic/Modules/Users/UserUpdateModule.cs
@@ -2,7 +2,9 @@
 using System.Net;
 using System.Net.Http;
 using JsonPatch;
+using MemeLord.DataObjects.Request;
 using MemeLord.Logic.Authentication;
+using MemeLord.Logic.Mapping.Users;
 using MemeLord.Logic.Repository;
 using MemeLord.Models;
 
@@ -10,18 +12,33 @@ namespace MemeLord.Logic.Modules.Users
 {
     public interface IUserUpdateModule
     {
+        HttpResponseMessage UpdateUser(UpdateUserRequest request);
         HttpResponseMessage UpdateUser(int id, JsonPatchDocument<User> userPatch);
     }
 
     public class UserUpdateModule : IUserUpdateModule
     {
         private readonly IUserRepository _userRepository;
+        private readonly IUpdateUserRequestMapper _requestMapper;
         private readonly HashManager _hashManager;
 
-        public UserUpdateModule(IUserRepository userRepository, HashManager hashManager)
+        public UserUpdateModule(IUserRepository userRepository, IUpdateUserRequestMapper requestMapper, HashManager hashManager)
         {
             _userRepository = userRepository;
+            _requestMapper = requestMapper;
             _hashManager = hashManager;
+        }
+
+        public HttpResponseMessage UpdateUser(UpdateUserRequest request)
+        {
+            if (request == null) return new HttpResponseMessage(HttpStatusCode.UnsupportedMediaType);
+            var originalUser = _userRepository.GetUserByCredentials(request.Username);
+            if (originalUser == null) return new HttpResponseMessage(HttpStatusCode.NotFound);
+            var newUser = _requestMapper.Map(request);
+            newUser.Id = originalUser.Id;
+            newUser.Hash = originalUser.Hash;
+            _userRepository.SaveUser(newUser);
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
         public HttpResponseMessage UpdateUser(int id, JsonPatchDocument<User> userPatch)
